f1ef84b2e78624a6cf549e4d81ab220b20753970
StefanieFanie <160854789+StefanieFanie@users.noreply.github.com>
2024-05-29 13:58:38 +0700

    Update laravelsi41
    Belajar DB Seeder dan Eloquent Relationship

--- app/Http/Controllers/ProdiController.php
+++ app/Http/Controllers/ProdiController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\DB;
+
 use Illuminate\Http\Request;
 
 class ProdiController extends Controller
@@ -13,5 +15,11 @@ class ProdiController extends Controller
 
         // return compact('fakultas','kampus');
         return view ('fakultas.index',compact('fakultas','kampus'));
+    }
+
+    public function allJoinFacade(){
+        $kampus = "Universitas Multi Data Palembang";
+        $result = DB::select('select mahasiswas.*,prodis.nama as nama_prodi from prodis,mahasiswas where prodis.id=mahasiswas.prodi_id');
+        return view('prodi.index');
     }
 }
